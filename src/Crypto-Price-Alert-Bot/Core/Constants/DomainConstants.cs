0473cead7bc49bca689788350e334e08bb5002e4
figurewindruin <donnamooreq568@gmail.com>
2026-08-26 13:42:21 -0700

    Initial commit: src/Crypto-Price-Alert-Bot/Core/Constants/DomainConstants.cs

---
+++ src/Crypto-Price-Alert-Bot/Core/Constants/DomainConstants.cs
@@ -0,0 +1,15 @@
+namespace CryptoPriceAlertBot.Core.Constants
+{
+    public static class DomainConstants
+    {
+        public const int DefaultPageSize = 50;
+        public const int MaxPageSize = 1000;
+        public const int DefaultRetryCount = 3;
+        public const int DefaultTimeoutMs = 30000;
+        public const string DefaultLogLevel = "Information";
+        public const string ConfigurationSectionName = "CryptoPriceAlertBot";
+        public const string HealthCheckEndpoint = "/health";
+        public const string MetricsEndpoint = "/metrics";
+        public const int CacheExpirationMinutes = 5;
+    }
+}
